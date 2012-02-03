71a8a05b418e031e8d349bd3039aa21374fd0550
Eric Rochester <err8n@virginia.edu>
2012-02-03 13:48:38 -0500

    Added d3 to the JS that gets loaded on every page.

---
+++ app/assets/javascripts/d3/index.js
@@ -0,0 +1,9 @@
+
+//= require d3/d3.min.js
+//= require d3/d3.chart.min.js
+//= require d3/d3.csv.min.js
+//= require d3/d3.geo.min.js
+//= require d3/d3.geom.min.js
+//= require d3/d3.layout.min.js
+//= require d3/d3.time.min.js
+
